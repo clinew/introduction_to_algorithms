5ec6349b5ff25ccf8dd0f6ee858de61c7124d649
Frotsnow (Wade T. Cline) <wadecline@hotmail.com>
2015-11-08 21:23:53 -0800

    Update frostsnow's e4_1-3.
    Allow user to use the book's array rather than a randomly-generated
    one.

--- frostsnow/e4_1-3.c
+++ frostsnow/e4_1-3.c
@@ -5,6 +5,8 @@
 
 // Arguments to the program.
 struct arguments {
+	// Use the book's array.
+	int book;
 	// Use brute-force rather than recursive method.
 	int brute;
 	// Array size.
@@ -18,11 +20,12 @@ int arguments_parse(struct arguments* arguments, int argc, char* argv[]) {
 	int ret;
 
 	// Set default arguments.
+	arguments->book = 0;
 	arguments->brute = 0;
 	arguments->count = 32;
 
 	// Parse arguments.
-	while ((ret = getopt(argc, argv, "bn:")) != -1) {
+	while ((ret = getopt(argc, argv, "bkn:")) != -1) {
 		// Parsing error.
 		if (ret == '?' || ret == ':') {
 			return -1;
@@ -31,6 +34,9 @@ int arguments_parse(struct arguments* arguments, int argc, char* argv[]) {
 		switch (ret) {
 		case 'b':
 			arguments->brute = 1;
+			break;
+		case 'k':
+			arguments->book = 1;
 			break;
 		case 'n':
 			arguments->count = atoi(optarg);
@@ -61,8 +67,9 @@ void arguments_usage(char* message) {
 	}
 
 	// Print usage message.
-	fprintf(stream, "USAGE: e4_1-3 [-b] [-n INT]\n\n");
+	fprintf(stream, "USAGE: e4_1-3 [-b] [-k] [-n INT]\n\n");
 	fprintf(stream, "  -b     Use brute-force rather than recursive algorithm\n");
+	fprintf(stream, "  -k     Use book's subarray instead of generated one. Overrides '-n'.\n");
 	fprintf(stream, "  -n INT Use array of size INT (default: 32)\n");
 
 	// Exit the program.
@@ -191,7 +198,6 @@ int main(int argc, char* argv[]) {
 	int sum;
 
 	// TODO:
-	// - Allow book's array.
 	// - Print brute to Graphviz.
 	// - Print recursive to Graphviz.
 	// - Time run.
@@ -205,14 +211,28 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Generate array.
+	if (arguments.book) {
+		arguments.count = 16;
+	}
 	array = malloc(sizeof(int) * arguments.count);
 	if (!array) {
 		fprintf(stderr, "Unable to allocate array.\n");
 		exit(EXIT_FAILURE);
 	}
 	srand(0xDEADBEEF);
+	if (arguments.book) {
+		// Use book's array.
+		array[0] = 13; array[1] = -3; array[2] = -25; array[3] = 20;
+		array[4] = -3; array[5] = -16; array[6] = -23; array[7] = 18;
+		array[8] = 20; array[9] = -7; array[10] = 12; array[11] = -5;
+		array[12] = -22; array[13] = 15; array[14] = -4; array[15] = 7;
+	} else {
+		// Randomly-generated array.
+		for (i = 0; i < arguments.count; i++) {
+			array[i] = (rand() % 64) - 32;
+		}
+	}
 	for (i = 0; i < arguments.count; i++) {
-		array[i] = (rand() % 64) - 32;
 		printf("%i ", array[i]);
 	}
 	printf("\n");
@@ -221,7 +241,7 @@ int main(int argc, char* argv[]) {
 	if (arguments.brute) {
 		// Brute-force.
 		sum = maxsubarray_brute(array, arguments.count, &low, &high);
-		printf("Brute: '%i','%i','%i', ", sum, low, high);
+		printf("Brute: '%i','%i','%i'.\n", sum, low, high);
 	} else {
 		// Recursive.
 		sum = maxsubarray_recur(array, 0, arguments.count - 1, &low, &high);
